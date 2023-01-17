68c27d10f2d6ace301d64434931e45c3e9916274
Juliya28 <98090213+Juliya28@users.noreply.github.com>
2023-01-17 14:22:18 +0300

    Update Calculator.java

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,2 +1,20 @@
-package org.example;public class Calculator {
+package org.example;
+
+public class Calculator {
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        double discountedAmount = 0;
+
+        if (purchaseAmount >= 0) {
+            if (discountAmount >= 0 && discountAmount <= 100) {
+                discountedAmount = purchaseAmount - (purchaseAmount * discountAmount) / 100;
+            }
+            else {
+                throw new ArithmeticException("Скидка должна быть в диапазоне от 0% до 100%");
+            }
+        }
+        else {
+            throw new ArithmeticException("Сумма покупки не должна быть отрицательной!!!");
+        }
+        return discountedAmount;
+    }
 }
